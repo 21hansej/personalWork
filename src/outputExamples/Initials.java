9584f951249b77f4412366b4fe3efc6a46e3e7f0
Jazlynn <21hansej@elmbrookstudents.org>
2018-09-13 14:02:24 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,18 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		
+		System.out.println("      J     GGGGG     H    H");
+		System.out.println("      J     G         H    H");
+		System.out.println("      J     G         H    H");
+		System.out.println("      J     G  GG     HHHHHH");
+		System.out.println(" J    J     G   G     H    H");
+		System.out.println(" JJJJJJ     GGGGG     H    H");
+
+
+
+	}
+
+}
