9844931b50713c57d703caffacf641e99c62d4a4
Jazlynn <21hansej@elmbrookstudents.org>
2018-10-04 13:27:35 -0500

    Car

---
+++ src/oop/Car.java
@@ -0,0 +1,28 @@
+package oop;
+
+//import statements at the top
+
+public class Car {
+
+	//instance fields - data storage
+	
+	
+	
+	//constructors - methods for constructing the objects
+	
+	
+	
+	//default constructor
+	
+	
+	
+	public Car()
+	{
+	//Statements to initialize the state of the object
+		
+		
+	}
+	//methods - things the object can do
+	
+	
+}
