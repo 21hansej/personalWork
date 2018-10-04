0a7c9046cb81c3548b77cf7901ecb875aa0d7dee
Jazlynn <21hansej@elmbrookstudents.org>
2018-10-04 14:10:05 -0500

    10/4/2018

---
+++ src/oop/Numbers.java
@@ -0,0 +1,31 @@
+package oop;
+
+public class Numbers {
+	
+	
+
+	public void sayNumber(double num)
+	{
+		
+		System.out.println("Your number is " + num);
+	}
+	
+	public void sayNumberPlus2(double num)
+	{
+		System.out.println("");
+	}
+	public void saySum(double first, double second)
+	{
+		System.out.println("answer=" +(first + second));
+		double third = 
+	}
+	public Numbers() {
+		
+
+			
+			
+		
+		
+	}
+	
+}
---
+++ src/oop/NumbersTest.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		Numbers bob = new Numbers();
+		
+		
+		bob.sayNumber(double.class);
+		bob.sayNumberPlus2(double.class);
+	
+
+	}
+
+}
