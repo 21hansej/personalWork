c5612712076ece3e25284fd3772c10867f41f10f
Jazlynn <21hansej@elmbrookstudents.org>
2018-10-04 13:27:52 -0500

    OOP Files

---
+++ src/oop/Dog.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Dog {
+	
+	
+	public Dog() {
+		
+		
+	}
+	
+	public void woof() {
+		System.out.println("Woof");
+	}
+	
+	public void attack(){
+		System.out.println("Grrrr");
+	}
+	
+
+}
---
+++ src/oop/Greeter.java
@@ -0,0 +1,31 @@
+package oop;
+
+//import statements at the top
+
+public class Greeter {
+
+	//instance fields - data storage
+	
+	
+	
+	//constructors - methods for constructing the objects
+	public void sayHello()
+	{
+		System.out.println("Hello.");
+	}
+	
+	
+	//default constructor
+	
+	
+	
+	public Greeter()
+	{
+	//Statements to initialize the state of the object
+		
+		
+	}
+	//methods - things the object can do
+	
+	
+}
