11919afc04271eb55bcf87098b267692d08b6453
Unknown <21hansej@elmbrookstudents.org>
2018-10-29 15:08:19 -0500

    Nested Loops

--- src/decisions/ForLoop.java
+++ src/decisions/ForLoop.java
@@ -8,15 +8,18 @@ public class ForLoop {
 		
 		int counter = 0;
 		int num = 0;
+		int sum = 0;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("What is your favorite number?");
-			num = input.nextInt();
+		//System.out.println("What is your favorite number?");
+			//num = input.nextInt();
 			
-		for (counter = 1; counter <= 10; counter++){
-			System.out.println(num + " Hello.");
+		for (counter = 1; counter <= 5; counter++){
+			System.out.println("Pick a number.");
+				num = input.nextInt();
+			sum = sum + num;
 		}
-		
+		System.out.println("The sum of the numbers entered is: " + sum);
 	}
 
 }
