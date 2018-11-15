b0981f52f87b23b744ec41e7b41bfe000f4d7153
Jazlynn <21hansej@elmbrookstudents.org>
2018-11-15 13:00:16 -0600

    Strings 3

--- src/strings/PasswordVerification.java
+++ src/strings/PasswordVerification.java
@@ -1,10 +1,28 @@
 package strings;
-
+import java.util.Scanner;
 public class PasswordVerification {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		boolean verified = false;
+		String password;
+		String verify;
+		
+		System.out.println("Enter a password.");
+		password = input.nextLine();
+		System.out.println("Verify password.");
+		verify = input.nextLine();
+		
+		while (!password.equals(verify)) {
+			System.out.println("Passwords do not match.");
+			System.out.println("Enter a password.");
+			password = input.nextLine();
+			System.out.println("Verify password.");
+			verify = input.nextLine();
+			
+		}
+		System.out.println("Verified");
 	}
 
 }
---
+++ src/strings/SocialSecurityNum.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.Scanner;
+
+public class SocialSecurityNum {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String ssc;
+		int index = 0;
+		
+		
+
+	}
+
+}
