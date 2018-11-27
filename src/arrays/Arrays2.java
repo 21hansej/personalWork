7f146d667695993905ba0da7291488ad02c22bcc
Jazlynn <21hansej@elmbrookstudents.org>
2018-11-27 15:03:07 -0600

    Arrays 2

---
+++ src/arrays/Arrays2.java
@@ -0,0 +1,16 @@
+package arrays;
+import java.util.Scanner;
+public class Arrays2 {
+
+	public static void main(String[] args) {
+		final int numScores = 250;
+		double[] test = new double[numScores];
+		Scanner input = new Scanner(System.in);
+		
+	System.out.println("The number of elements in the array: " + test.length);
+	for(int i = 0; i < test.length; i++){
+		
+	}
+	}
+
+}
