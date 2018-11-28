564415edf2fff9ccbe59e4699acfbf0bbe68e778
Jazlynn <21hansej@elmbrookstudents.org>
2018-11-28 14:48:45 -0600

    Rectangle Update

--- src/arrays/Arrays3.java
+++ src/arrays/Arrays3.java
@@ -9,6 +9,8 @@ public class Arrays3 {
 		int length = 0;
 		int min = 10;
 		int max = 56;
+		int smallest = 3136;
+		int smallestIndex = 0;
 		for(int counter = 0; counter < 100; counter++){
 			rectangle[counter] = new Rectangle();
 			rectangle[counter].setLength(length = min + generator.nextInt(max - min + 1));
@@ -17,6 +19,12 @@ public class Arrays3 {
 			System.out.println("Length: " + length);
 			System.out.println("Area: " + rectangle[counter].area());
 			System.out.println("");
+			if(rectangle[counter].area() < smallest){
+				smallest = rectangle[counter].area();
+				smallestIndex = counter;
+				rectangle[0] = rectangle[smallestIndex];
+			}
 		}
+		System.out.println("Smallest: " + smallest + " at index: " + smallestIndex);
 	}
 }
