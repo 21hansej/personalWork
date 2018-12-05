ef6e4f395ef48c2937d898a1496490a43769fc50
Jazlynn <21hansej@elmbrookstudents.org>
2018-12-05 15:05:26 -0600

    Array List Update

--- src/arrays/FriendArray.java
+++ src/arrays/FriendArray.java
@@ -1,9 +1,21 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FriendArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		ArrayList<Friend> friends = new ArrayList<Friend>();
+		
+		Friend one = new Friend();
+		Friend two = new Friend();
+		Friend three = new Friend();
+		friends.add(three);
+		friends.add(two);
+		friends.add(one);
+
 
 	}
 
