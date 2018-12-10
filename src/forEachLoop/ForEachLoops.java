fde92d7e34be065e83c6f932123ab1ff6a56990e
Jazlynn <21hansej@elmbrookstudents.org>
2018-12-10 14:42:12 -0600

    For Each Loops

---
+++ src/forEachLoop/ForEachLoops.java
@@ -0,0 +1,24 @@
+package forEachLoop;
+import java.util.Scanner;
+public class ForEachLoops {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int highest = 0;
+		
+		System.out.println("How many employees work here?");
+		int employees = input.nextInt();
+		
+		int[] wages = new int[employees];
+		
+		for(int i : wages){
+			System.out.println("What is the emplyee's wage?");
+			int wage = input.nextInt();
+			if(wage > highest){
+				highest = wage;
+			}
+		}
+		System.out.println("Highest wage: " + highest);
+	}
+
+}
